403094567482503dab31bcf93642b599d128f50c
visualstudio <visualstudio@122-PC25>
2019-01-25 21:03:32 -0500

    final commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,16 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp4
+
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
     class Countryside
     {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village InitialVillage = this.Maeland;
+        }
     }
+
     class Village
     {
-        Village PreviousVillage;
-        Village NextVillage;
-        String Villagename;
-        bool 
-
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
     }
 }
